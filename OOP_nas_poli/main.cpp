d09f00c21baa43310653c86d725112a6f031ae1c
Mickle36 <maltsev0492@list.ru>
2024-07-13 16:05:47 +1100

    ctreat Equilateral triangle

--- OOP_nas_poli/main.cpp
+++ OOP_nas_poli/main.cpp
@@ -4,6 +4,7 @@
 #include "Quadrangle.hpp"
 #include "Right_triangle.hpp"
 #include "Isosceles_triangle.hpp"
+#include "Equilateral_triangle.hpp"
 
 int main()
 {
@@ -13,11 +14,14 @@ int main()
 	Quadrangle quadr(10, 20, 30, 40, 50, 60, 70, 80);
 	Right_triangle rig_triang(10, 20, 30, 50, 60);
 	Isosceles_triangle iso_triangle(10, 20, 50, 60);
+	Equilateral_triangle equ_triangle(20);
+
 	
 	simple_fig.about_me();
 	triang.about_me();
 	quadr.about_me();
 	rig_triang.about_me();
 	iso_triangle.about_me();
+	equ_triangle.about_me();
 	return 0;
 }
