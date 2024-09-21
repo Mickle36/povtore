4329847d3b10273ab23c2a1f2e9bd909573c96ae
Mickle36 <maltsev0492@list.ru>
2024-09-21 19:02:26 +1100

    correct Macros

--- Preproc_Macros/Func.hpp
+++ Preproc_Macros/Func.hpp
@@ -3,6 +3,7 @@
 
 #include <iostream>
 #define MODE 3
+#define SUB(a, b) ((a) - (b))
 
 void add();
 void start_game();
--- Preproc_Macros/main.cpp
+++ Preproc_Macros/main.cpp
@@ -4,5 +4,13 @@ int main()
 {
 	setlocale(LC_ALL, "ru");
 	
-	start_game();
+	//start_game();
+
+	int a = 6;
+	int b = 5;
+	int c = 2;
+
+	std::cout << SUB(a, b) << std::endl;
+	std::cout << SUB(a, b) * c << std::endl;
+	std::cout << SUB(a, b + c) * c << std::endl;
 }
